80aa3f82faf191ec039a2b3eadd70e8eca766e7c
Israelmbuyu77 <israelmbuyu77@gmail.com>
2024-09-17 21:12:56 +0200

    Initial commit: setup project structure and linked JavaScript file

---
+++ app.js
@@ -0,0 +1,21 @@
+
+// Function to get computer's random choice
+function getComputerChoice() {
+    const randomNumber = Math.floor(Math.random()*3);
+    if (randomNumber == 0) {
+        return "rock";
+    } else if (randomNumber == 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
+console.log(getComputerChoice());
+
+// Function to get humans's random choice
+function getHumanChoice() {
+    user_choice = parseInt(prompt("Enter your choice: "))
+    
+}
